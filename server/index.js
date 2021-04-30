402553834278837a494efc26abad6acf6e34c634
Cristiano Amici <amicicristiano@gmail.com>
2021-04-29 22:38:59 -0500

    Integrate Reddit API
    - Keep mocked data for simplicity during development
    - Include endpoint to provide oauth token

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const path = require("path");
 const express = require("express");
+const axios = require("axios");
+
 const app = express();
 const publicPath = path.join(__dirname, "..", "build");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+app.get("/reddit/auth", async (req, res) => {
+  const params = new URLSearchParams();
+  params.append("grant_type", "password");
+  params.append("username", process.env.REDDIT_USERNAME);
+  params.append("password", process.env.REDDIT_PASSWORD);
+
+  const { data } = await axios.post(
+    "https://www.reddit.com/api/v1/access_token",
+    params,
+    {
+      auth: {
+        username: process.env.CLIENT_ID,
+        password: process.env.CLIENT_SECRET,
+      },
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+
+  return res.json({ accessToken: data.access_token });
+});
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
--- src/api/reddit.ts
+++ src/api/reddit.ts
@@ -4,10 +4,24 @@ import {
   RedditListingInterface,
 } from "../interfaces/reddit";
 
-// TODO: Replace mocked response with actual data fetch from Reddit
+const getAuthToken = async (): Promise<string> => {
+  const { data } = await axios.get("/reddit/auth");
+  return data.accessToken;
+};
+
 export default async function getTopPosts(): Promise<RedditListingInterface[]> {
+  if (process.env.NODE_ENV === "development") {
+    const { data } = await axios.get<RedditListingResponseInterface>(
+      "data/top.json"
+    );
+
+    return data.data.children;
+  }
+
+  const accessToken = await getAuthToken();
   const { data } = await axios.get<RedditListingResponseInterface>(
-    "data/top.json"
+    "https://oauth.reddit.com/top",
+    { headers: { Authorization: `Bearer ${accessToken}` } }
   );
 
   return data.data.children;
